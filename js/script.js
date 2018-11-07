d2df981393c8a9612fec8e15f0073adbe7108a46
AlexK <alkremn@gmail.com>
2018-11-06 17:10:06 -0700

    Added implementation of checking name and email

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $('#name').focus();
 $('#other-title').hide();
 $('#colors-js-puns').hide();
 let total = 0;
-
+$('#payment option[value ="select_method"').attr('disabled', 'disabled');
 $('#title').change( e => {
     if(e.target.value === 'other'){
         $('#other-title').show();
@@ -107,6 +107,150 @@ $('.activities').change( e => {
 });
 
 
+$('#payment').change( e=> {
+    const method = e.target.value;
+    const creditCardDiv = $('#credit-card');
+    if(method === 'credit card'){
+        hideAllPaymentDivs(creditCardDiv);
+        creditCardDiv.show();
+    }
+    if (method === 'paypal'){
+        hideAllPaymentDivs(creditCardDiv);
+        creditCardDiv.next().show();
+    }
+    if(method === 'bitcoin'){
+        hideAllPaymentDivs(creditCardDiv);
+        creditCardDiv.next().next().show();
+    }
+});
 
+function hideAllPaymentDivs(paymentDiv){
+    paymentDiv.hide();
+    paymentDiv.next().hide();
+    paymentDiv.next().next().hide();
+}
+
+$('#name').on('input', ()=> {
+    nameCheck();
+});
+
+$('#mail').on('input', () =>{
+    emailCheck();
+});
+$('#cc-num').on('input', () => {
+    ccNumCheck();
+});
+
+$('#zip').on('input', () => {
+    zipCheck();
+});
+
+$('#cvv').on('input', () => {
+    cvvCheck();
+});
+
+function nameCheck(){
+    const name = $('#name');
+    if(name.val() ===''){
+        name.css('border-color', 'coral');
+        return false;
+    }
+    else{
+        name.css('border-color', '#c1deeb');
+        return true;
+    }
+}
+
+function emailCheck(){
+    const mail = $('#mail');
+    const valid = isEmailValid(mail.val());
+    if(mail.val() === '' || !valid){
+        mail.css('border-color', 'coral');
+        return false;
+    }
+    else{
+        mail.css('border-color', '#c1deeb');
+        return true;
+    }
+}
+function ccNumCheck(){
+    const ccNum = $('#cc-num');
+    const valid = isCCNumValid(ccNum.val());
+    if(ccNum.val() ==='' || !valid){
+        ccNum.css('border-color', 'coral');
+        return false;
+    }
+    else{
+        ccNum.css('border-color', '#c1deeb');
+        return true;
+    }
+}
+
+function zipCheck(){
+    const zip = $('#zip');
+    const valid = isZipValid(zip.val());
+    if(zip.val() ==='' || !valid){
+        zip.css('border-color', 'coral');
+        return false;
+    }
+    else{
+        zip.css('border-color', '#c1deeb');
+        return true;
+    }
+}
+
+function cvvCheck(){
+    const cvv = $('#cvv');
+    const valid = isCvvValid(cvv.val());
+    if(cvv.val() ==='' || !valid){
+        cvv.css('border-color', 'coral');
+        return false;
+    }
+    else{
+        cvv.css('border-color', '#c1deeb');
+        return true;
+    }
+}
+
+function isEmailValid(mail){
+    return /^\w+@\w+.(com|net|org)$/.test(mail);
+}
+
+function isCCNumValid(ccNum){
+    return /^\d{13,16}$/.test(ccNum);
+}
+
+function isZipValid(zip){
+    return /^\d{5}$/.test(zip);
+}
+function isCvvValid(cvv){
+    return /^\d{3}$/.test(cvv);
+}
+
+$('form').on('submit', (e) =>{
+    e.preventDefault();
+    const nameValid = nameCheck();
+    const emailValid = emailCheck();
+    const checkBoxChecked = validateCheckBox();
+    if(nameValid && emailValid && checkBoxChecked){
+       if($('#payment').val() === 'credit card'){
+            validateCreditCard();
+        }
+    }
+
+    
+});
+
+function validateCheckBox(){
+    const activities =  $('input[type="checkbox"]');
+    
+    let isChecked = false;
+    activities.each( function(index){
+        if($(this).prop('checked')){
+            isChecked = true;
+        }
+    });
+    return isChecked
+}
 
 
